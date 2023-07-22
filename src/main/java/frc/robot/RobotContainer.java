4746a0f5953a3db8586f465b1eb87851538fdf72
Josh <eppersonjoshua435@gmail.com>
2023-07-21 19:52:54 -0700

    got the turn motor moving IRL

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,23 +6,17 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.TeleopSwerve;
 import frc.robot.subsystems.DuckSwerveDrive;
 
 public class RobotContainer {
-  private final CommandPS4Controller driverJoystick = new CommandPS4Controller(Constants.DriveControllerCharacteristics.port);
+  private final CommandXboxController driverJoystick = new CommandXboxController(Constants.DriveControllerCharacteristics.port);
 
-  private final DuckSwerveDrive swerveSubsystem = new DuckSwerveDrive(
-    Constants.SwerveDriveCharacteristics.chassisDimensionsMeters, Constants.SwerveDriveCharacteristics.ksAngularVolts,
-    Constants.SwerveDriveCharacteristics.ksLinearVolts, Constants.SwerveDriveCharacteristics.kvLinearVoltMeters, Constants.SwerveDriveCharacteristics.kaLinearVoltMeters,
-    Constants.CANIDS.SwerveModules.FrontLeft.turnMotor, Constants.CANIDS.SwerveModules.FrontLeft.driveMotor, Constants.CANIDS.SwerveModules.FrontLeft.CANcoder,
-    Constants.CANIDS.SwerveModules.FrontRight.turnMotor, Constants.CANIDS.SwerveModules.FrontRight.driveMotor, Constants.CANIDS.SwerveModules.FrontRight.CANcoder,
-    Constants.CANIDS.SwerveModules.BackLeft.turnMotor, Constants.CANIDS.SwerveModules.BackLeft.driveMotor, Constants.CANIDS.SwerveModules.BackLeft.CANcoder,
-    Constants.CANIDS.SwerveModules.BackRight.turnMotor, Constants.CANIDS.SwerveModules.BackRight.driveMotor, Constants.CANIDS.SwerveModules.BackRight.CANcoder
-  );
+  private final DuckSwerveDrive swerveSubsystem = new DuckSwerveDrive();
 
   public RobotContainer() {
+    
     swerveSubsystem.setDefaultCommand(new TeleopSwerve(swerveSubsystem, driverJoystick::getLeftX, driverJoystick::getLeftY, driverJoystick::getRightX));
     configureBindings();
   }
--- src/main/java/frc/robot/common/DuckSwerveModule.java
+++ src/main/java/frc/robot/common/DuckSwerveModule.java
@@ -1,69 +1,57 @@
 package frc.robot.common;
 
-import com.ctre.phoenix.sensors.CANCoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class DuckSwerveModule extends SubsystemBase {
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.RobotController;
+
+public class DuckSwerveModule {
     private final CANSparkMax m_turnMotor;
+    private double angleGoal;
     private final CANSparkMax m_driveMotor;
-    private final CANCoder absoluteShaftEncoder;
-    private SwerveModuleState m_moduleState; //represents the velocity state
+    private final AnalogInput absoluteEncoder;
+    private final boolean absoluteEncoderIsInverted;
     
-    private final RelativeEncoder m_turnEncoder; //in terms of angular positon on output shaft
-    private final RelativeEncoder m_driveEncoder; //in terms of meters per second
+    private final RelativeEncoder m_turnEncoder;
 
     //PF controller (supply static ff as plant)
     private final SparkMaxPIDController m_turnController;
-    private final double kTurnStaticVoltage;
+    private final double KS_VOLTS_TURNING;
 
-    //PF controller
-    private final SparkMaxPIDController m_driveController;
-    private final SimpleMotorFeedforward m_drivePlant;
-
-    public DuckSwerveModule(int CAN_ID_turnMotor, int CAN_ID_driveMotor, int CAN_ID_CANcoder, double turnkS, double driveKs, double driveKv, double driveKa) {
+    public DuckSwerveModule(int CAN_ID_TURN_MOTOR, int CAN_ID_DRIVE_MOTOR, int ANALOG_PWM_PORT_NUMBER, double KS_VOLTS_TURNING, boolean absoluteEncoderInverted) {
         //motor configuration on the burned flash, change via dashboard
-        this.m_turnMotor = new CANSparkMax(CAN_ID_turnMotor, MotorType.kBrushless);
-        this.m_driveMotor = new CANSparkMax(CAN_ID_driveMotor, MotorType.kBrushless);
-        this.m_moduleState = new SwerveModuleState(0, Rotation2d.fromDegrees(0));
+        this.m_turnMotor = new CANSparkMax(CAN_ID_TURN_MOTOR, MotorType.kBrushless);
+        this.m_driveMotor = new CANSparkMax(CAN_ID_DRIVE_MOTOR, MotorType.kBrushless);
 
         this.m_turnEncoder = m_turnMotor.getEncoder(); //the motor might need to be inverted if it doesnt rotate CCW+
         this.m_turnController = m_turnMotor.getPIDController();
         this.m_turnController.setFeedbackDevice(m_turnEncoder);
-
-        this.m_driveEncoder = m_driveMotor.getEncoder();
-        this.m_driveController = m_driveMotor.getPIDController();
-        this.m_driveController.setFeedbackDevice(m_driveEncoder);
         
-        this.kTurnStaticVoltage = turnkS;
-        this.m_drivePlant = new SimpleMotorFeedforward(driveKs, driveKv, driveKa); //calculate these gains as meters per second
-        this.absoluteShaftEncoder = new CANCoder(CAN_ID_CANcoder); //make sure to apply the magnet offset in phoenix tuner
-
-        this.m_turnEncoder.setPosition(this.absoluteShaftEncoder.getPosition());
+        this.KS_VOLTS_TURNING = KS_VOLTS_TURNING;
+        
+        this.absoluteEncoderIsInverted = absoluteEncoderInverted;
+        this.absoluteEncoder = new AnalogInput(ANALOG_PWM_PORT_NUMBER);
+        this.m_turnEncoder.setPosition(0);
+        this.angleGoal = 0; //should be the same as the home angle
     }
 
-    public void updateModuleState(SwerveModuleState velocityVector) {
-        m_moduleState = SwerveModuleState.optimize(velocityVector, Rotation2d.fromDegrees(m_turnEncoder.getPosition()*360.0));
+    public void setTargetAngle(double angle) {
+        angleGoal = angle;
     }
-
     public void applyControlAlgorithms() {
-        if (m_moduleState != null) {
-            m_turnController.setReference(m_moduleState.angle.getDegrees()/360.0, ControlType.kPosition, 0, kTurnStaticVoltage);
-        
-            //closed loop drive
-            m_driveController.setReference(m_moduleState.speedMetersPerSecond, ControlType.kVelocity, 0, 
-            m_drivePlant.calculate(m_moduleState.speedMetersPerSecond, m_moduleState.speedMetersPerSecond, 0.02)); //rio runs stable at 50hz
-        }
+        m_turnController.setReference(angleGoal/360.0, ControlType.kPosition, 0, this.KS_VOLTS_TURNING);
     }
-    public SwerveModulePosition getPosition() {
-        return new SwerveModulePosition(m_driveEncoder.getVelocity(), Rotation2d.fromDegrees(m_turnEncoder.getPosition()));
+    public void setTurnSpeed(double speed) {
+        m_turnMotor.set(speed);
+    }
+    public void setDriveSpeed(double speed) {
+        m_driveMotor.set(speed);
+    }
+    public double getAnalogEncoderPosition() {
+        return (absoluteEncoderIsInverted ? -1.0 : 1.0) * ((absoluteEncoder.getAverageVoltage() / RobotController.getVoltage5V()) * (Math.PI * 2) - Math.PI);
     }
 }
